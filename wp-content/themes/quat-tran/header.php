e92ac7d2b65562f228671d893ab5a90094e65a5a
longpv <longpv@codluck.com>
2024-10-18 21:54:32 +0700

    ô tìm kiếm

--- wp-content/themes/quat-tran/header.php
+++ wp-content/themes/quat-tran/header.php
@@ -45,6 +45,19 @@
 
 					<div class="col-6 col-xl-9">
 						<div class="header__navInner">
+							<div class="form_search">
+								<form role="search" method="get" action="<?php echo home_url('/'); ?>">
+									<input type="text" name="s" placeholder="Nhập từ khóa tìm kiếm..."
+										value="<?php the_search_query(); ?>" required />
+									<button type="submit">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+											<path fill="#ffffff"
+												d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+										</svg>
+									</button>
+								</form>
+							</div>
+
 							<!-- menu PC -->
 							<?php
 							if (has_nav_menu('menu-1')) {
