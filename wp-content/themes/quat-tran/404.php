788083e973f0eb351a61685833c0b8e583591e17
longpv <longpv@codluck.com>
2024-10-19 22:36:44 +0700

    css

--- wp-content/themes/quat-tran/404.php
+++ wp-content/themes/quat-tran/404.php
@@ -9,52 +9,17 @@
 
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'quat-tran' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'quat-tran' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'quat-tran' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$quat_tran_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'quat-tran' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$quat_tran_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
+<section class="secSpace page404">
+	<div class="container">
+		<div class="editor">
+			<h2 style="text-align:center;font-weight:bold;color:#C72127;font-size:80px;line-height:1;">
+				<?php _e('404', 'quat-tran'); ?>
+			</h2>
+			<p style="text-align:center;">
+				<?php _e('Page not found', 'quat-tran'); ?>
+			</p>
+		</div>
+	</div>
+</section>
 <?php
 get_footer();
